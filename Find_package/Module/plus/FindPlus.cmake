2829056f3cc8c093fa2af15d5b78cb27147be89f
Xiaohui Dong <tianfuyuming@outlook.com>
2026-05-06 02:00:50 +0800

    tmp

--- Find_package/Module/plus/FindPlus.cmake
+++ Find_package/Module/plus/FindPlus.cmake
@@ -2,7 +2,8 @@ set(plus_root ${CMAKE_CURRENT_SOURCE_DIR}/plus/out)
 
 find_library(
     Plus_LIB
-    NAMES libplus.a
+    # NAMES libplus.a
+    NAMES libplus.dll.a
     PATHS ${plus_root}/lib
     NO_DEFAULT_PATH
 )
